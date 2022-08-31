bc0c1c04abc127ba914a67c924397b5cc9881899
andres <adiaz14@live.com>
2022-08-31 06:34:36 -0500

    fix: Activity No. 5 - last settings -functionality and styles-

--- src/components/FormCompoment.jsx
+++ src/components/FormCompoment.jsx
@@ -30,7 +30,7 @@ const FormCompoment = () => {
 
     }
 
-    //Delete object of list
+    //Delete object in list
     const deleteUser = (index) => {
         //Get items to remove
         let nameToRemove = list[index].name;
@@ -41,9 +41,11 @@ const FormCompoment = () => {
 
 
     return (
-        <div className="container-main container">
+        /*Begin of main container*/
+        <div className="container-main container col-12 col-sm-12 col-md-8 col-lg-7 col-xl-6 col-xxl-5">
             <hr />
             <h2 className="secundary-titles">Registration Form</h2>
+            {/* Begin of form container */}
             <div className="form-container container-fluid">
                 <form onSubmit={saveUser} className="container">
                     <input type="text"
@@ -57,28 +59,30 @@ const FormCompoment = () => {
                         onChange={(e) => setLastname(e.target.value)}
                     />
                     <div className='d-grid gap2'>
-                        <button type='submit' className='btn btn btn-success m-3'>Save</button>
+                        <button type='submit' className='btn-success btn btn btn-success m-3'>Save</button>
                     </div>
                 </form>
             </div>
-
+            {/* End of form container */}
             <hr />
-
             <h2 className="secundary-titles">List of registred users</h2>
-            <div className="list-container">
-                <ul className="list-group">
+            {/* Begin of list container */}
+            <div className="list-container container col-12 col-sm-8 col-md-8 col-lg-7 col-xl-7 col-xxl-7">
+                <ul className="list-group container">
                     {
                         list.map((item, index) => (
                             <li className='list-element mb-2 bg-light text-dark' key={index}>
                                 <div className="col-8"><span>{item.name} {item.lastname}</span></div>
-                                <div key={index} className="col-4"><button type='submit' onClick={() => deleteUser(index)} id="btn-delete" className='btn btn-danger m-3' color="danger">Delete</button></div>
+                                <div key={index} className="col-4"><button type='submit' onClick={() => deleteUser(index)} id="btn-delete" className='btn-delete btn btn-danger m-1' color="danger">Delete</button></div>
                             </li>
                         ))
                     }
                 </ul>
             </div>
+            {/* End of list container */}
             <hr />
         </div>
+        /*End of main container*/
     )
 }
 export default FormCompoment
